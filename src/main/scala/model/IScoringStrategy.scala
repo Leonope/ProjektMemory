bb34e1ef4155066dd96ea3b986884a676d8e3442
eidummy@htwg-konstanz.de <eidummy@htwg-konstanz.de>
2025-10-19 16:53:32 +0200

    play server implement

--- src/main/scala/model/IScoringStrategy.scala
+++
@@ -1,7 +0,0 @@
-package model
-
-trait IScoringStrategy {
-  def calculateScore(currentScore: Int, matchFound: Boolean, consecutiveMatches: Int): Int
-}
-
-
--- src/test/scala/model/test.scala
+++
@@ -1,8 +0,0 @@
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-
-class test extends AnyFlatSpec with Matchers {
-  "An Test example" should "be true" in {
-    true shouldBe true
-  }
-}
