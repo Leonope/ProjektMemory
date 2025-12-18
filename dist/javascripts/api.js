1c930b6959cd2d7ccaea9773d77dcb2360b2713e
steelortus <3wa.wf2e@gmx.de>
2025-12-18 11:06:43 +0100

    vue deployment to Github Pages

--- dist/javascripts/api.js
+++
@@ -1,6 +0,0 @@
-// src/api.js
-const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:9000';
-
-export function fetchData() {
-  return fetch(`${API_URL}/cmd`).then(res => res.json());
-}
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ProjektMemory/'
+    : '/',
+  pwa: {
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      navigateFallback: '/index.html',
+      runtimeCaching: [
+        // Offline-Fallback für Navigation (wenn index.html nicht verfügbar wäre)
+      ]
+    }
+  }
 })
+/*
 module.exports = {
   pwa: {
     workboxPluginMode: 'GenerateSW',
@@ -13,3 +26,8 @@ module.exports = {
     }
   }
 }
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/a/'
+    : '/'
+}*/
