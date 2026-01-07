d483eec5d5b8fa0696fa4ab4882874cacd221bfa
steelortus <3wa.wf2e@gmx.de>
2026-01-07 14:03:50 +0100

    update router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
-// src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../components/main_bootstrap.vue'
 import MemoryGameView from '../views/MemoryGameView.vue'
 
@@ -17,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/ProjektMemory'),
+  history: createWebHashHistory(), // << use hash mode
   routes,
 })
 
